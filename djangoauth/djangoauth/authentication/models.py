68eedf1f86d411373da06c8a1a6d95d1569fbaaf
abhay <abhay.singh@rootinfosol.com>
2016-03-19 19:13:11 +0530

    make a model and serializer

--- djangoauth/djangoauth/authentication/models.py
+++ djangoauth/djangoauth/authentication/models.py
@@ -4,28 +4,6 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
-class Account(AbstractBaseUser):
-	email = models.EmailField(unique=True)
-	username = models.CharField(max_length=40,unique=True)
-	last_name = models.CharField(max_length=40,unique=True)
-
-	is_admin = models.BooleanField(default=False)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-
-
-	objects = AccountManager()
-
-	REQUIRED_FIELDS = ['username']
-
-	def __unicode__(self):
-		return self.email
-
-	def get_full_name(self):
-		return ' '.join([self.first_name,self.last_name])
-
-	def get_short_name(self):
-		return self.first_name
 
 class AccountManager(BaseUserManager):
     def create_user(self,email,password=None,**kwargs):
@@ -50,6 +28,32 @@ class AccountManager(BaseUserManager):
     	account.save()
 
     	return account
+
+
+class Account(AbstractBaseUser):
+	email = models.EmailField(unique=True)
+	username = models.CharField(max_length=40,unique=True)
+	last_name = models.CharField(max_length=40,unique=True)
+
+	is_admin = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+	objects = AccountManager()
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+
+	def __unicode__(self):
+		return self.email
+
+	def get_full_name(self):
+		return ' '.join([self.first_name,self.last_name])
+
+	def get_short_name(self):
+		return self.first_name
+
 
 
 
