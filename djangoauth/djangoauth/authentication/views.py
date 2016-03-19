0f2825d23c08bc170457026216eb249092496b15
abhay <abhay.singh@rootinfosol.com>
2016-03-19 19:37:24 +0530

    make a model and serializer and view

--- djangoauth/djangoauth/authentication/views.py
+++ djangoauth/djangoauth/authentication/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from authentication.permissions import IsAccountOwner
+from authentication.serializers import AccountSerializer
 
 # Create your views here.
+
+class AccountViewSet(viewsets.ModelViewSet):
+	lookup_field = 'username'
+	queryset = Account.objects.all()
+	serializer_class = AccountSerializer
+
+	def get_permission(self):
+		if self.request.method in permissions.SAFE_METHODS:
+			return (permissions.AllowAny(),)
+
+		if self.request.method == 'POST':
+			return (permissions.AllowAny(),)
+
+		return (permissions.IsAuthenticated(),IsAccountOwner())
+
